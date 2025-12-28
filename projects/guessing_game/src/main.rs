02bf71b6c4bac2152370685e01a40abe7ba01ab2
KJJDSA <kjjdsa@gmail.com>
2025-12-28 19:55:44 +0900

    2장 추리게임 완성

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -18,12 +18,9 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        // parse 의 Result 결과물을 바꿀 때, expect로 무조건 프로그램을 끝내는 것이 아니라 계속 진행할 수 있도록 에러처리 하기
-        // let guess: u32 = guess.trim().parse().expect("Please type a number!");
         let guess: u32 = match guess.trim().parse() {
-            // match 는 parse의 Result 를 받을 것이므로, Result의 구성인 두 열거형을 추가하면 분기할 수 있다.
             Ok(number) => number,
-            Err(_) => continue, // _ === 포괄값(catch-all). Err에 담긴 어떤 값이든 매칭 할 수 있게된다. 
+            Err(_) => continue,
         };
 
         println!("You guessed: {guess}");
