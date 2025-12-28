5985a6995a0c56af3cbbc90a0c2948003107b2ed
KJJDSA <kjjdsa@gmail.com>
2025-12-28 19:47:02 +0900

    반복문으로 여러번 추리하기

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -9,27 +9,27 @@ fn main() {
 
     println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess.");
+    loop {
+        // loop : 무한루프를 발생시킨다.
+        println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    // 섀도잉 : guess라는 변수 이름을 타입만 바꿔 재사용하고 싶을 때 자주 사용되는 기능. 이전에 있던 guess값을 새로운 값으로 가리는(shadow) 것
-    let guess: u32/* 32비트 부호없는 정수(양수) = 작은 양수 */ = guess
-        .trim() //read_line을 끝내기 위해 엔터를 누르고 개행문자가 추가되므로 필요
-        .parse() // 문자열을 다른타입으로 바꿔줌. 변수 옆 콜론(:)과 타입 명시 필요
-        .expect("Please type a number!");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    println!("You guessed: {guess}");
+        println!("You guessed: {guess}");
 
-    // guess 와 secret_nunber를 비교 / cmp: 비교가능한 모든것을 호출 가능
-    match guess.cmp(&secret_number) {
-        // 1. cmp 가 비교한 값을 반환. 예) 50 vs 38 => Ordering::Greater
-        Ordering::Less => println!("Too small!"), // 패턴이 매치 안되므로 첫번째 갈래 무시
-        Ordering::Greater => println!("Too bug!"), // 이 갈래와 연관된 코드가 실행
-        Ordering::Equal => println!("You win!"),  // 마지막 갈래는 확인하지 않음
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too bug!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
