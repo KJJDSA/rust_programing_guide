bd78bae89965a8a471b8a96cd548f32d3cfed310
KJJDSA <kjjdsa@gmail.com>
2026-01-06 21:26:35 +0900

    섀도잉

--- projects/variables/src/main.rs
+++ projects/variables/src/main.rs
@@ -1,13 +1,14 @@
 fn main() {
-    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3; // 상수
-    /* 
-    1. 반드시 대문자를 사용하고 타입을 표기
-    2. 런타임에서 값을 확인할 수 있는 값은 넣을 수 없음 (예: x + 1)
-    3. 하드코딩된 값을 다른 개발자에게 유용하게 정보로 넘길 수 있음 
-    4. 선언된 스코프 내에서 프로그램이 동작하는 전체 시간 동안 유효하다. 
-     */
-    let mut x = 5; 
-    println!("The value of x is: {x}");
-    x = 6; 
-    println!("The value of x is: {x}");
+    let x = 5;
+
+    // 섀도잉 : 기존의 값을 잠시 가린다. 섀도잉은 스코프가 끝나면 걷혀진다. 
+    // 즉 같은 스코프에서 변한 것은 영구적으로 변한 것이라 봐야한다. 
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}"); // 12
+    }
+
+    println!("The value of x is: {x}"); // 다시 6으로 돌아왔다. 
 }
